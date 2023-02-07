c35e9c2e58ef3c1bcc499885a5ee0ae0e4b892ca
seolacarrano <seola6@gmail.com>
2023-02-07 16:53:39 -0500

    add EventListener

---
+++ main.js
@@ -0,0 +1,52 @@
+// DOM elements 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
+// Generate event listener
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+})
+
+// // Generate password function 
+// function generatePassword(lower, upper, number, symbol, length) {
+//     // 1. Initialize password variable
+//     // 2. Filter out unchecked types 
+//     // 3. Loop over the length, call generator function for each type 
+//     // 4. Add final password to the password variable and return it
+// }
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
